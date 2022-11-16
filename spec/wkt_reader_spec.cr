8f37aa8a94b256444eadb5df8adfe206f6553edf
Kolesár András <kolesar@kolesar.hu>
2022-11-16 05:43:24 +0100

    free allocated memory

--- spec/wkt_reader_spec.cr
+++ spec/wkt_reader_spec.cr
@@ -9,6 +9,9 @@ describe Geos do
       geometry = Geos::Lib.geoswkt_reader_read_r(context, reader, "POINT(1 1.5)");
       writer = Geos::Lib.geoswkt_writer_create_r(context);
       wkt = Geos::Lib.geoswkt_writer_write_r(context, writer, geometry)
+      Geos::Lib.geoswkt_writer_destroy_r(context, writer);
+      Geos::Lib.geoswkt_reader_destroy_r(context, reader);
+      Geos::Lib.geos_finish_r(context);
       content = String.new(wkt)
       content.should eq "POINT (1.0000000000000000 1.5000000000000000)"
     end
